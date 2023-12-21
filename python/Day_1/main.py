a98fcb7dc8e16565f99db5724055049fbfb0eae6
gurinderjeet kaur <36306330+gjkaur@users.noreply.github.com>
2023-12-21 22:03:30 +0530

    create repos

---
+++ python/Day_1/main.py
@@ -0,0 +1,13 @@
+# use of print function
+
+print("Hello World!") # strings are written within quotes
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')") # use of single quotes and brackets inside double quotes
+
+print("Hello world!\nHello world!\nHello world!") # new line escape character \n
+print("Hello"+" gurinder") # string concatenation
+print("Hello"+" "+"gurinder") # space concatenation
+
+# take care of indentation
